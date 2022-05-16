40300438e3fa3f468d9275c22b161ed2ce83f4ef
kjhwa0713 <84023652+kjhwa0713@users.noreply.github.com>
2022-05-16 15:36:06 +0900

    5week

--- api/index.js
+++ api/index.js
@@ -3,8 +3,6 @@ import posts from "./posts";
 
 
 var router=Router();
-
-
 router.use("/", posts);
 
 export default router;
--- api/posts.js
+++ api/posts.js
@@ -1,73 +1,103 @@
 import { Router } from "express";
 
-var router=Router();
+const router=Router();
 
-let datas = [
-		{
-			"id": 1,
-			"content": "대충 글 내용",
-			"writer": 1,
-		},
-    {
-			"id": 2,
-			"content": "대충 글 내용",
-			"writer": 1,
-		}
-	]
-let nextId=datas[datas.length-1].id + 1
+let members= [];
+let datas = [];
+
+let nextUser=members.length + 1
+let nextPost=datas.length + 1
+
 router.get("/posts", (req, res) => {
   res.json({data:datas});
 });
 
-router.get('/post', (req, res) => {  
-  const index = datas.findIndex(data => data.id === req.body.id);
-  if (index === -1) {
+router.get('/post/:id', (req, res) => {
+  let {id}=req.params;
+  if (!datas[id-1]) {
     return res.json({
       error: "Post not exist",
     });
   }
-  res.json({data:datas.filter(data => data.id === req.body.id)[0]});
+  res.json({data:datas[id-1]});
   
 });
 
-
-router.post('/posts', (req, res) => { 
+router.post('/posts', (req, res) => {
   datas.push({
-    id: nextId++,
-    content: req.body.content,
-    writer: req.body.id
+    id: nextPost++,
+    content:req.body.content,
+    writer: req.header("id")
   });
-  res.json({data:{post:{id:(nextId-1)}}});
+  res.json({data:{post:{id:(nextPost-1)}}});
 });
 
-
 router.put('/posts', (req, res) => {
-  const index = datas.findIndex(data => data.id === req.body.postId);
-  if (req.body.userId !== datas[index].writer) { 
+  let postId = Number(req.header("postId"));
+  let userId = req.header("userId");
+  if (userId !== datas[postId-1].writer) { 
     return res.json({
       error: "Cannot modify post"
     });
   }
 
-  datas[index] = {
-    id: req.body.postId,
-    content: req.body.content,
-    writer: req.body.userId
+  datas[postId-1] = {
+    id: postId,
+    content:req.body.content,
+    writer: userId
   };
-  res.json({data:{id:req.body.postId}});
+  res.json({data:{id:postId}});
 });
 
 router.delete('/posts', (req, res) => {
-  const index = datas.findIndex(data => data.id === req.body.postId);
-  if (req.body.userId !== datas[index].writer) { 
+  let postId = Number(req.header("postId"));
+  let userId = req.header("userId");
+  if (userId !== datas[postId-1].writer) { 
     return res.json({
       error: "Cannot delete post"
     });
   }
-  datas = datas.filter(data => data.id !== req.body.postId);
+  datas = datas.filter(data => data.id !== postId);
   res.json({
     "data": "Successfully deleted"
   });
+});
+
+router.get('/auth', (req, res) => {
+  res.json(members);
+});
+
+// 회원가입
+router.post('/auth/register', (req, res) => {
+  let email = req.header("email");
+  let password = req.header("password");
+  const index = members.findIndex(member => member.email === email);
+
+  if (index !== -1) {
+    return res.json({
+      error: "User already exist",
+    });
+  }
+  members.push({
+    id: nextUser++,
+    email: email,
+    password: password
+  });
+  res.json({data:{user:{id:nextUser-1}}});
+});
+
+// 로그인
+router.post('/auth/login', (req, res) => {
+  let email = req.header("email");
+  let password = req.header("password");
+  const index = members.findIndex(member => member.email === email);
+  
+  if (index === -1) {
+    return res.json({
+      error: "User not exist",
+    });
+  }
+  res.json({data:{user:{id:members[index].id}}});
 });
 
 export default router;
